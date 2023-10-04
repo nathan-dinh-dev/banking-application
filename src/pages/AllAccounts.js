d92b7fe4d11ffcc697078dccbd7983c8bce8cff6
Nathan Dinh <nathanxdinhx97@gmail.com>
2023-10-04 01:12:48 -0700

    Added All Accounts Page

---
+++ src/pages/AllAccounts.js
@@ -0,0 +1,39 @@
+import styles from "./AllAccounts.module.css";
+import AuthContext from "../store/auth-context";
+import { useContext } from "react";
+
+const AllAccounts = () => {
+  const ctx = useContext(AuthContext);
+  const users = ctx.users;
+
+  return (
+    <main className={styles["flex-wrap"]}>
+      <table className={styles["styled-table"]}>
+        <thead>
+          <tr>
+            <th>Customer</th>
+            <th>Account Type</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user}>
+                <td>{`${user.firstName} ${user.lastName}`}</td>
+                <td>Checking</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+                <td>{`$${user.money}`}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+
+export default AllAccounts;
